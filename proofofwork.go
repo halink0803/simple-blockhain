5254053ef42f677e67ded3798309b509b5e7aba9
halink0803 <halink0803@gmail.com>
2018-02-04 21:11:13 +0700

    update pow

--- proofofwork.go
+++ proofofwork.go
@@ -1,4 +1,16 @@
-import "math/big"
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+var (
+	maxNonce = math.MaxInt64
+)
 
 const targetBits = 24
 
@@ -9,19 +21,42 @@ type ProofOfWork struct {
 
 func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
-	target.Lsh(target, unit(256-targetBits))
+	target.Lsh(target, uint(256-targetBits))
 	pow := &ProofOfWork{b, target}
 	return pow
 }
 
 func (pow *ProofOfWork) prepareData(nonce int) []byte {
-    data := bytes.Join(
-        [][]byte{
-            pow.block.PrevBlockHash,
-            pow.block.Data,
-            IntToHex(pow.block.Timestamp),
-            IntToHex(int64(targetBits)),
-            IntToHex(int64(nonce)),
-        }
-    )
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\n\n")
+	return nonce, hash[:]
 }
