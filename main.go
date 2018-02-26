d08e92315f1a7ac9db799909d08875a7cea5921d
halink0803 <halink0803@gmail.com>
2018-02-26 17:01:17 +0700

    update log format with logrus and filename

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 )
@@ -11,24 +12,31 @@ var logger = logrus.New()
 
 // DecorateRuntimeContext appends line, file and function context to the logger
 func DecorateRuntimeContext(logger *logrus.Entry) *logrus.Entry {
-	if pc, file, line, ok := runtime.Caller(1); ok {
-		fName := runtime.FuncForPC(pc).Name()
-		return logger.WithField("file", file).WithField("line", line).WithField("func", fName)
+	if _, file, line, ok := runtime.Caller(1); ok {
+		file := strings.Split(file, "/")
+		return logger.WithField("file", file[len(file)-1]).WithField("line", line)
 	} else {
 		return logger
 	}
 }
 
 func initLogger() {
-	// logger = DecorateRuntimeContext
 	logger.Formatter = new(logrus.JSONFormatter)
 	logger.Formatter = new(logrus.TextFormatter)
 	logger.Formatter.(*logrus.TextFormatter).DisableTimestamp = false
 	logger.Level = logrus.DebugLevel
 	logger.Out = os.Stdout
+	// log.SetOutput(&lumberjack.Logger{
+	// 	Filename:   "./foo.log",
+	// 	MaxSize:    1, // megabytes
+	// 	MaxBackups: 3,
+	// 	MaxAge:     28,   //days
+	// 	Compress:   true, // disabled by default
+	// })
 }
 func main() {
 	initLogger()
+	DecorateRuntimeContext(logrus.NewEntry(logger)).Info("Test file name")
 	cli := CLI{}
 	cli.Run()
 }
